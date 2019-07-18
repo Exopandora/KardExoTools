c54cbeab14a13a02b158549217eb20bb35bf360a
Marcel Konrad <mkonrad98@alice-dsl.net>
2019-07-18 12:59:06 +0200

    1.14.3-2.38

--- src/main/java/exopandora/kardexo/kardexotools/data/Config.java
+++ src/main/java/exopandora/kardexo/kardexotools/data/Config.java
@@ -35,7 +35,7 @@ import net.minecraft.command.CommandSource;
 
 public class Config
 {
-	public static final String VERSION = "1.14.3-2.37";
+	public static final String VERSION = "1.14.3-2.38";
 	
 	//** CONFIGURABLE VALUES **//
 	
