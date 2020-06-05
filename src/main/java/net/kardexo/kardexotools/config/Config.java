d44e8603a94a1dab70afb83ce98dad8eb215752d
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-06-05 16:45:26 +0200

    Bump version

--- src/main/java/net/kardexo/kardexotools/config/Config.java
+++ src/main/java/net/kardexo/kardexotools/config/Config.java
@@ -32,7 +32,7 @@ import net.minecraft.command.CommandSource;
 
 public class Config
 {
-	public static final String VERSION = "1.15.2-2.39.2";
+	public static final String VERSION = "1.15.2-2.39.3";
 	
 	//** CONFIGURABLE VALUES **//
 	
