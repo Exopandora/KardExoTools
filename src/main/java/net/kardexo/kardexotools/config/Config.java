7ec28bde2d8c5e14447a2652f8ee3ed005426041
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-07-20 00:01:25 +0200

    Bump version

--- src/main/java/net/kardexo/kardexotools/config/Config.java
+++ src/main/java/net/kardexo/kardexotools/config/Config.java
@@ -35,7 +35,7 @@ import net.minecraft.util.math.BlockPos;
 
 public class Config
 {
-	public static final String VERSION = "1.16.1-2.41.7";
+	public static final String VERSION = "1.16.1-2.41.8";
 	
 	//** CONFIGURABLE VALUES **//
 	
