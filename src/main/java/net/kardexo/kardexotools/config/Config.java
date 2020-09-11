e68948b2613528bb2d96f189a49868c68586a044
Marcel Konrad <mkonrad98@alice-dsl.net>
2020-09-11 22:44:51 +0200

    Update to 1.16.3

--- src/main/java/net/kardexo/kardexotools/config/Config.java
+++ src/main/java/net/kardexo/kardexotools/config/Config.java
@@ -35,7 +35,7 @@ import net.minecraft.util.math.BlockPos;
 
 public class Config
 {
-	public static final String VERSION = "1.16.2-2.41.8";
+	public static final String VERSION = "1.16.3-2.41.8";
 	
 	//** CONFIGURABLE VALUES **//
 	
