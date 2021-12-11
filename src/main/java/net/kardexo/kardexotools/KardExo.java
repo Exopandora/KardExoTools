c9d8b24f6ab5f5b11a08a340daa2751f079570b7
Marcel Konrad <mkonrad98@alice-dsl.net>
2021-12-11 16:29:00 +0100

    Update to 1.18.1

--- src/main/java/net/kardexo/kardexotools/KardExo.java
+++ src/main/java/net/kardexo/kardexotools/KardExo.java
@@ -57,7 +57,7 @@ import net.minecraft.world.level.block.Blocks;
 
 public class KardExo
 {
-	public static final String VERSION = "1.18-2.44";
+	public static final String VERSION = "1.18.1-2.44";
 	public static final Logger LOGGER = LogManager.getLogger("KardExo");
 	
 	private static final File CONFIG_DIRECTORY = new File("config/kardexotools");
