5152a6926396eeebf00bead1625a05def11e8b02
Marcel Konrad <mkonrad98@alice-dsl.net>
2021-12-18 16:16:06 +0100

    Replace reflection with accesswidener

--- src/main/java/net/kardexo/kardexotools/util/CommandUtils.java
+++ src/main/java/net/kardexo/kardexotools/util/CommandUtils.java
@@ -1,7 +1,5 @@
 package net.kardexo.kardexotools.util;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.EnumSet;
 import java.util.UUID;
 
@@ -33,18 +31,8 @@ public class CommandUtils
 	
 	public static int teleport(CommandSourceStack source, ServerPlayer player, ServerLevel level, BlockPos position) throws CommandSyntaxException
 	{
-		try
-		{
-			Method method = TeleportCommand.class.getDeclaredMethods()[3];
-			method.setAccessible(true);
-			method.invoke(null, new Object[] {source, player, level, position.getX() + 0.5F, position.getY(), position.getZ() + 0.5F, EnumSet.noneOf(ClientboundPlayerPositionPacket.RelativeArgument.class), player.getYRot(), player.getXRot(), null});
-			player.connection.send(new ClientboundSetExperiencePacket(player.experienceProgress, player.totalExperience, player.experienceLevel));
-		}
-		catch(SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
-		{
-			e.printStackTrace();
-		}
-		
+		TeleportCommand.performTeleport(source, player, level, position.getX() + 0.5F, position.getY(), position.getZ() + 0.5F, EnumSet.noneOf(ClientboundPlayerPositionPacket.RelativeArgument.class), player.getYRot(), player.getXRot(), null);
+		player.connection.send(new ClientboundSetExperiencePacket(player.experienceProgress, player.totalExperience, player.experienceLevel));
 		return 1;
 	}
 	
