a6e676ca1b167337dc07cd1a9b59e2bb2e93af6b
Marcel Konrad <mkonrad98@alice-dsl.net>
2022-08-29 19:04:53 +0200

    Bump version

--- src/main/java/net/kardexo/kardexotools/KardExo.java
+++ src/main/java/net/kardexo/kardexotools/KardExo.java
@@ -55,7 +55,7 @@ import net.minecraft.world.level.block.Blocks;
 
 public class KardExo
 {
-	public static final String VERSION = "1.19.2-2.46.6";
+	public static final String VERSION = "1.19.2-2.46.7";
 	public static final Logger LOGGER = LogManager.getLogger("KardExo");
 	
 	private static final File CONFIG_DIRECTORY = new File("config/kardexotools");
