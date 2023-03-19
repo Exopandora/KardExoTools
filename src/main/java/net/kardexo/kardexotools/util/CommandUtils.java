24a1f099b643e7d39acec61dc8ec7762a764e904
Exopandora <mkonrad98@alice-dsl.net>
2023-03-19 21:19:50 +0100

    Update to 1.19.4

--- src/main/java/net/kardexo/kardexotools/util/CommandUtils.java
+++ src/main/java/net/kardexo/kardexotools/util/CommandUtils.java
@@ -10,11 +10,11 @@ import net.minecraft.Util;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.game.ClientboundPlayerPositionPacket;
 import net.minecraft.network.protocol.game.ClientboundSetExperiencePacket;
 import net.minecraft.server.commands.TeleportCommand;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.RelativeMovement;
 
 public class CommandUtils
 {
@@ -30,7 +30,7 @@ public class CommandUtils
 	
 	public static int teleport(CommandSourceStack source, ServerPlayer player, ServerLevel level, BlockPos position) throws CommandSyntaxException
 	{
-		TeleportCommand.performTeleport(source, player, level, position.getX() + 0.5F, position.getY(), position.getZ() + 0.5F, EnumSet.noneOf(ClientboundPlayerPositionPacket.RelativeArgument.class), player.getYRot(), player.getXRot(), null);
+		TeleportCommand.performTeleport(source, player, level, position.getX() + 0.5F, position.getY(), position.getZ() + 0.5F, EnumSet.noneOf(RelativeMovement.class), player.getYRot(), player.getXRot(), null);
 		player.connection.send(new ClientboundSetExperiencePacket(player.experienceProgress, player.totalExperience, player.experienceLevel));
 		return 1;
 	}
