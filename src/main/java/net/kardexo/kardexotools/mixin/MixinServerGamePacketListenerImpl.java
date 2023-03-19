f6708102e5f6592b546513fc034c271f690fc4d8
Exopandora <mkonrad98@alice-dsl.net>
2023-03-19 21:19:33 +0100

    Fix base protection not working when placing a block on the side of another block at the edge of the base

---
+++ src/main/java/net/kardexo/kardexotools/mixin/MixinServerGamePacketListenerImpl.java
@@ -0,0 +1,29 @@
+package net.kardexo.kardexotools.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.protocol.game.ServerboundUseItemOnPacket;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.entity.player.Player;
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public class MixinServerGamePacketListenerImpl
+{
+	@Redirect
+	(
+		method = "handleUseItemOn",
+		at = @At
+		(
+			value = "INVOKE",
+			target = "Lnet/minecraft/server/level/ServerLevel;mayInteract(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;)Z"
+		)
+	)
+	public boolean handleUseItemOn_mayInteract(ServerLevel level, Player player, BlockPos pos, ServerboundUseItemOnPacket packet)
+	{
+		return level.mayInteract(player, pos) && level.mayInteract(player, pos.relative(packet.getHitResult().getDirection()));
+	}
+}
