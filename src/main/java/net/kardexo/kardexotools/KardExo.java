96380ed5cc4783570fa475ae88def34d0591ee81
Exopandora <mkonrad98@alice-dsl.net>
2023-04-04 22:27:51 +0200

    Bump version

--- src/main/java/net/kardexo/kardexotools/KardExo.java
+++ src/main/java/net/kardexo/kardexotools/KardExo.java
@@ -56,7 +56,7 @@ import net.minecraft.world.level.block.Blocks;
 
 public class KardExo
 {
-	public static final String VERSION = "1.19.3-2.49";
+	public static final String VERSION = "1.19.4-2.50";
 	public static final Logger LOGGER = LogManager.getLogger("KardExo");
 	
 	private static final File CONFIG_DIRECTORY = new File("config/kardexotools");
